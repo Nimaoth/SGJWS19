c6704f74ad5689f4147339614281a059384b35b7
Nico Otremba <nico.otremba@mmi-media.de>
2019-12-08 00:53:13 +0100

    icicle is now trigger

--- SGJWS19/Assets/Scripts/Icicle.cs
+++ SGJWS19/Assets/Scripts/Icicle.cs
@@ -8,7 +8,6 @@ public class Icicle : MonoBehaviour
     public bool DidDamage = false;
 
     private Vector3 originalPosition;
-    private bool respawning;
     private new Rigidbody2D rigidbody;
 
     void Start()
@@ -18,34 +17,30 @@ public class Icicle : MonoBehaviour
         StartCoroutine(Drop());
     }
 
-    private IEnumerator Respawn()
+    private void Respawn()
     {
-        if (respawning)
-            yield break;
-        respawning = true;
-        yield return new WaitForSeconds(2.0f);
-
         rigidbody.isKinematic = true;
         rigidbody.position = originalPosition;
         rigidbody.rotation = 0;
         rigidbody.velocity = Vector2.zero;
         rigidbody.angularVelocity = 0;
-        respawning = false;
-
-        StartCoroutine(Drop());
     }
 
     private IEnumerator Drop()
     {
-        yield return new WaitForSeconds(Random.Range(2.0f, 4.0f));
-        rigidbody.isKinematic = false;
-        DidDamage = false;
-    }
-
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            StartCoroutine(Respawn());
+        while (true) {
+            Respawn();
+            yield return new WaitForSeconds(2.0f);
+            rigidbody.isKinematic = false;
+            DidDamage = false;
+            yield return new WaitForSeconds(4.0f);
         }
     }
+
+//     private void OnCollisionEnter2D(Collision2D other) {
+//         if (other.gameObject.CompareTag("Ground"))
+//         {
+//             StartCoroutine(Respawn());
+//         }
+//     }
 }
